b15941a292d370d4c784e14a327ca0bd0b2dffb0
ConniStein <cvillagranpoblete1994@gmail.com>
2016-06-23 12:24:19 -0400

    avance ejercicios javascript

---
+++ ComenzandoJavascript06-06-16/Javascript/js2/tiposdedatos.js
@@ -0,0 +1,61 @@
+//Ejercicio #1: División de números
+/*var numero1 = parseFloat(prompt("Ingrese un número"));
+var numero2 = parseFloat(prompt("Ingrese otro número"));
+var division = numero1 /numero2;
+alert("El resultado de esta división es: " + division);
+//si se dividen dos cadenas de texto el resultado es NaN (not a number)
+//si se divide un número por 0 el resultado es "infinity"*/
+
+//Ejercicio #2: Conversión de edad
+/*var nombre = prompt("Ingresa tu nombre");
+var edad = parseInt(prompt("Ingresa tu edad"));
+var diasVividos = edad * 365;
+alert("Sr/Sra. " + nombre + " , usted ha vivido: " + diasVividos + " días");*/
+
+//Ejercicio #3: Formato de output
+/*var nombre = prompt("Ingresa tu nombre");
+var apellido = prompt("Ingresa tu primer apellido");
+var poblacion = prompt("Ingresa la poblacion en que vives");
+document.write("=============================" + "<br>" +"Hola " + nombre + " " + apellido + "<br>");
+document.write("Adiós habitante de " + poblacion + "<br>" + "=============================")*/
+
+//Ejercicio #4: Contadores
+/*var numeroPrincipal = parseInt(prompt("Ingrese un número"));
+var primerContador = numeroPrincipal + 5;
+var segundoContador = primerContador + 21; 
+var tercerContador = segundoContador - 4;
+alert("Numero introducido: " + numeroPrincipal + "\n" + "Contadores: " + primerContador +" " + 
+	segundoContador + " " + tercerContador);*/
+
+//Ejercicio #5: Producto escalar
+
+
+//Ejercicio #6: Calcular el punto medio
+/*var x1 = parseInt(prompt("Ingrese el valor de x1"));
+var y1 = parseInt(prompt("Ingrese el valor de y1"));
+var x2 = parseInt(prompt("Ingrese el valor de x2"));
+var y2 = parseInt(prompt("Ingrese el valor de y2"));
+var resuelveX = (x1 + x2) / 2;
+var resuelveY = (y1 + y2) / 2;
+alert("El punto medio del segmento" + "\n" + "de extremos, los puntos:" + "\n" + "(" + x1 +"," + y1 + ")" + 
+	" y " + "(" + x2 + "," + y2 + ") es: (" + resuelveX + "," + resuelveY ")");
+//tiene algo mal la sintaxis*/
+
+//Ejercicio #7: Area del circulo
+var primerDato = prompt("¿Ingresará el valor del diámetro o del radio? (ingrese su respuesta en minúsculas");
+var diametroRadio = ["diametro", "radio"];
+var radio;
+var diametro;
+var area = 0;
+
+if (diametroRadio = "diametro") {
+	diametro = parseFloat(prompt("Ingrese el valor del diámetro del circulo"));
+	var deDiametroARadio = diametro / 2;
+	area = 3.14 * (radio*radio);
+	alert("El area de este circulo es: " + area);
+}
+else if (diametroRadio ="radio") {
+	radio = parseFloat(prompt("Ingrese el valor del radio del circulo"));
+	area = 3.14 * (radio*radio);
+	alert("El area de este circulo es: " + area);
+};
