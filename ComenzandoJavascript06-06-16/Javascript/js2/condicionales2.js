02c2a9f05a34b41a09d523e98d8e60bcaff8ac3a
ConniStein <cvillagranpoblete1994@gmail.com>
2016-06-24 14:26:03 -0400

    avances!

---
+++ ComenzandoJavascript06-06-16/Javascript/js2/condicionales2.js
@@ -0,0 +1,52 @@
+//Ejercicio #1: Completando condiciones
+/*var numero1 = 5;
+var numero2 = 8;
+
+if(numero1 < numero2) { 
+    alert("numero1 no es mayor que numero2"); 
+}
+
+if(numero2 >= 0){
+  alert("numero2 es positivo");
+}
+
+if(numero1 < 0){
+  alert("numero1 es negativo o distinto de cero");
+}
+
+if((numero1 + 1) !=numero2){
+  alert("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
+}*/
+
+//Ejercicio #2: Factorial de un numero
+
+
+//Ejercicio #3: Operaciones matemáticas
+/*var numero1 = 0;
+var numero2 = 0;
+var numero1 = parseInt(prompt("Ingrese el primer número"));
+var numero2 = parseInt(prompt("Ingrese el segundo número"));
+if (numero1 > numero2) {
+	var suma = numero1 + numero2;
+	var resta = numero1 - numero2;
+	alert("La suma de los dos numeros es: " + suma + "; y la resta de los dos números es: " + resta);
+}
+else if (numero1 === numero2) {
+	alert("Los dos números son iguales. No se realizará ninguna operación");
+}
+else{
+	var multiplicacion = numero1 * numero2;
+	//var division = (numero1 / numero2);
+	alert("Estos dos números multiplicados tienen como resultado: " + multiplicacion);
+};
+//la division no funciona*/
+
+//Ejercicio #4: Saldo bancario
+//WUAT
+
+//Ejercicio #5: Repetir un texto
+var texto = prompt("Ingrese el texto que desea repetir");
+var repeticion = parseInt(prompt("Ingrese el numero de veces que desea repetir el texto"));
+for (var i = 0; repeticion < 0  ; repeticion++) {
+	alert(texto);
+};
--- ComenzandoJavascript06-06-16/Javascript/js2/tiposdedatos.js
+++ ComenzandoJavascript06-06-16/Javascript/js2/tiposdedatos.js
@@ -1,3 +1,6 @@
+//FALTA EJERCICIO 5 Y EJERCICIO 6 Y 7 ESTÁN MALOS
+
+
 //Ejercicio #1: División de números
 /*var numero1 = parseFloat(prompt("Ingrese un número"));
 var numero2 = parseFloat(prompt("Ingrese otro número"));
@@ -38,11 +41,12 @@ var y2 = parseInt(prompt("Ingrese el valor de y2"));
 var resuelveX = (x1 + x2) / 2;
 var resuelveY = (y1 + y2) / 2;
 alert("El punto medio del segmento" + "\n" + "de extremos, los puntos:" + "\n" + "(" + x1 +"," + y1 + ")" + 
-	" y " + "(" + x2 + "," + y2 + ") es: (" + resuelveX + "," + resuelveY ")");
-//tiene algo mal la sintaxis*/
+	" y " + "(" + x2 + "," + y2 + ") es: (" + resuelveX + "," + resuelveY + ")" );*/
+//tiene algo mal la sintaxis
+
 
 //Ejercicio #7: Area del circulo
-var primerDato = prompt("¿Ingresará el valor del diámetro o del radio? (ingrese su respuesta en minúsculas");
+/*var primerDato = prompt("¿Ingresará el valor del diámetro o del radio? (ingrese su respuesta en minúsculas");
 var diametroRadio = ["diametro", "radio"];
 var radio;
 var diametro;
@@ -51,11 +55,36 @@ var area = 0;
 if (diametroRadio = "diametro") {
 	diametro = parseFloat(prompt("Ingrese el valor del diámetro del circulo"));
 	var deDiametroARadio = diametro / 2;
-	area = 3.14 * (radio*radio);
+	area = 3.14 * (deDiametroARadio*deDiametroARadio);
 	alert("El area de este circulo es: " + area);
 }
-else if (diametroRadio ="radio") {
+else if (diametroRadio = "radio") {
 	radio = parseFloat(prompt("Ingrese el valor del radio del circulo"));
 	area = 3.14 * (radio*radio);
 	alert("El area de este circulo es: " + area);
-};
+};*/
+//este igual tiene algo malo
+
+//Ejercicio #8: Area del triángulo
+/*var a = prompt("Escribe la base:")
+var b = prompt("Escribe la altura:")
+alert("Área= "+ (a*b)/2);*/
+
+//Ejercicio #9: Calcular determinante de segundo orden
+/*var numero1 = parseInt(prompt("Ingrese el primer elemento de la diagonal principal"));
+var numero2 = parseInt(prompt("Ingrese el primer elemento de la diagonal secundaria"));
+var numero3 = parseInt(prompt("Ingrese el segundo elemento de la diagonal principal"));
+var numero4 = parseInt(prompt("Ingrese el segundo elemento de la diagonal secundaria"));
+var A = (numero1* numero3) - (numero2*numero4);
+alert("El determinante de segundo orden de esta matriz es: " + A);*/
+
+//Ejercicio #10: Formato determinante de segundo orden
+/*var numero1 = parseInt(prompt("Ingrese el primer elemento de la diagonal principal"));
+var numero2 = parseInt(prompt("Ingrese el primer elemento de la diagonal secundaria"));
+var numero3 = parseInt(prompt("Ingrese el segundo elemento de la diagonal principal"));
+var numero4 = parseInt(prompt("Ingrese el segundo elemento de la diagonal secundaria"));
+alert("Su matriz se verá de la siguiente manera: " + "\n" + 
+	"A = [ " + numero1 +" " + numero2  + "\n"
+	+ numero4 + " " + numero3 + "]");
+//el tab no funciona en los alert (?)*/
+
